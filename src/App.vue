e7f040b9f239069cebbcfd336836438da2ef9943
nathan-ngp <nathan_piran@estudante.sc.senai.br>
2020-04-14 22:01:54 -0300

    Feito!

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <to-do-form @alteraTexto="adicionaTarefa($event)"/>
+    <to-do-list :tarefas="tarefas"/>
+  </div>
+</template>
+
+<script>
+import ToDoForm from "./components/ToDoForm.vue"
+import ToDoList from "./components/ToDoList.vue"
+export default {
+  name: 'App',
+  components: {
+    ToDoForm,
+    ToDoList
+  }, data: function(){
+    return{
+      tarefas:[
+        {descricao:"Matemática"},
+        {descricao:"Biologia"},
+      ]
+    }
+  },methods:{
+    adicionaTarefa:function(event){
+      this.tarefas.push({descricao:event.texto})
+    },
+  }
+}
+</script>
+
+<style>
+</style>
